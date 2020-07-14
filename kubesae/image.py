922b0caf98a43398f274413c1f40af65677e91c0
Dmitriy Chukhin <dchukhin@gmail.com>
2020-07-14 10:45:54 -0400

    add a write_docker_env_file() command to write a docker_env_file based on a template

--- kubesae/image.py
+++ kubesae/image.py
@@ -4,7 +4,63 @@ Provides utilities to build and push Docker images.
 """
 
 import invoke
+import subprocess
 from colorama import Style
+
+
+@invoke.task()
+def write_docker_env_file(c):
+    """
+    Write a docker_env_file, based on the c.docker_env_file_template configuration.
+
+    Usage: inv image.write_docker_env_file
+
+    Config:
+
+        docker_env_file_template_path: Path to the template for the docker_env_file,
+            which should have variable names, and commands to get their values, like:
+            MOST_RECENT_GIT_REVISION=git rev-parse --short HEAD
+            GIT_BRANCH=git rev-parse --abbrev-ref HEAD
+    """
+    # This is the config variable that should define the path to the docker_env_file template.
+    docker_env_file_template_var_name = "docker_env_file_template_path"
+
+    if c.config.get(docker_env_file_template_var_name):
+        # This is the content that will be written to the docker_env_file at the
+        # end of this method.
+        docker_env_file_content = ""
+
+        # Open the template file, and loop through its lines, running the command
+        # for each line.
+        with open(c.config[docker_env_file_template_var_name], "r") as docker_env_file_template:
+            for line in docker_env_file_template.read().split("\n"):
+                # If the line is truthy (not empty), then split it up into the
+                # variable name and the command. Then run the command, and add
+                # its result into the docker_env_file_content.
+                # For example, if the line is DEALER_REVISION=git rev-parse --short HEAD,
+                # then var_name will be DEALER_REVISION, and command will be
+                # "git rev-parse --short HEAD".
+                if line:
+                    # If the line doesn't have an equal sign (for example, a comment),
+                    # then just add the line to the docker_env_file_content.
+                    if line.count("=") == 0:
+                        docker_env_file_content += f"{line}\n"
+                    else:
+                        var_name = line.split("=")[0]
+                        command = line[len(var_name) + 1:]
+                        result = subprocess.run(
+                            command,
+                            capture_output=True,
+                            shell=True
+                        ).stdout.decode('utf-8').rstrip()
+                        docker_env_file_content += f"{var_name}={result}\n"
+
+        # Write the docker_env_file_content into the docker_env_file.
+        with open("docker_env_file", "w") as docker_env_file:
+            docker_env_file.write(docker_env_file_content)
+        print("docker_env_file written")
+    else:
+        raise KeyError(f"Please set '{docker_env_file_template_var_name}' in config file")
 
 
 @invoke.task()
@@ -91,6 +147,7 @@ def stop(c):
 
 
 image = invoke.Collection("image")
+image.add_task(write_docker_env_file, "write-docker-env-file")
 image.add_task(generate_tag, "tag")
 image.add_task(build_image, "build")
 image.add_task(push_image, "push")
