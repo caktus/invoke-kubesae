65ea78620615e3647cefe04a3ef5d1a841ce3466
Colin Copeland <copelco@caktusgroup.com>
2021-03-18 17:03:54 -0400

    ignore tests dir in sdist

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='invoke-kubesae',
-    version='0.0.11',
-    packages=find_packages(),
-    url='https://github.com/caktus/invoke-kubesae',
-    author='Caktus Group',
-    author_email='',
-    description='',
+    name="invoke-kubesae",
+    version="0.0.11",
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/caktus/invoke-kubesae",
+    author="Caktus Group",
+    author_email="",
+    description="",
     install_requires=[
-        'boto3>=1.16',
-        'invoke>=1.4',
-        'colorama>=0.4',
-        'ansible>=2.9',
+        "boto3>=1.16",
+        "invoke>=1.4",
+        "colorama>=0.4",
+        "ansible>=2.9",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-    ]
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+    ],
 )
